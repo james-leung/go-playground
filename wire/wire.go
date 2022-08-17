2c2a9076e8da5e35a7583c22749543e3afd5e52d
James Leung <48014394+james-leung@users.noreply.github.com>
2022-08-17 22:11:08 +0100

    Migration to postgres container

---
+++ wire/wire.go
@@ -0,0 +1,14 @@
+//+build wireinject
+
+package main
+
+import "github.com/google/wire"
+
+func NewEventNumber() int {
+	return 1
+}
+
+func InitializeEvent(phrase string) (Event, error) {
+	wire.Build(NewEvent, NewGreeter, NewMessage)
+	return Event{}, nil
+}
